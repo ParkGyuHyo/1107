c4c613f494e6375680a5964e652b16cd5b930840
ParkGyuHyo <55084952+ParkGyuHyo@users.noreply.github.com>
2019-11-07 15:16:31 +0900

    Add files via upload

---
+++ exam01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+//void print_stars();
+
+void print_stars()
+{
+	printf("*************************\n");
+}
+
+int main()
+{
+	print_stars();
+	printf("Hello C world\n");
+	print_stars();
+		
+	return 0;
+}
